3bc50f088895aa0be59264d580ceb0a38b1e659f
Jakub Jarina <49947841+kvet-divizny@users.noreply.github.com>
2025-01-26 11:11:34 +0100

    Prubezny commit

---
+++ complexity.py
@@ -0,0 +1,49 @@
+import requests
+from dataclasses import dataclass
+from typing import Optional
+from xml.etree import ElementTree
+
+@dataclass
+class Game():
+    id: int
+    name: str
+    weight: Optional[float] = None
+
+def req_games(gameid):
+    base_url = "https://boardgamegeek.com/xmlapi2/thing"
+    payload = {
+        "id": gameid,
+        "stats": 1,
+    }
+
+    r = requests.get(url = base_url, params=payload)
+
+    print(r.url)
+    print(r.status_code)
+    return r
+    
+def parse_response(node):
+    game = Game(
+        id = node.get("id"),
+        name = node.find("name").get("value"),
+        weight = node.find("statistics/ratings/averageweight").get("value")
+    )
+    return game
+
+def main():
+    id = "311715,355326"
+    games = []
+
+    resp = req_games(id)
+    tree = ElementTree.fromstring(resp.content)
+
+    for node in tree:
+        game = parse_response(node)
+        games.append(game)
+
+    for game in games:
+        print(game.name, game.weight)
+
+
+if __name__ == "__main__":
+    main()
